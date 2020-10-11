25954a776cd93211c3526c7d1f5a456cc1434efd
Andrey Dolya <dolya7kk@gmail.com>
2020-10-11 18:45:23 +0300

    contact menu bug fixed

--- js/offCanvas.js
+++ js/offCanvas.js
@@ -1,4 +1,5 @@
 function openCanvas(){
+    document.querySelector('.open-sidebar').classList.toggle('offcanvas-btn-open')
     document.querySelector('.offcanvas-btn').classList.toggle('offcanvas-btn-open');
     document.querySelector('.offcanvas-aside').classList.toggle('offcanvas-aside-open');    
 }
